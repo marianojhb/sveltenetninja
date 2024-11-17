94706bfa32136543084e41e3824d4bc51b4429f5
Mariano Belgrano <mbelgrano@gmail.com>
2024-11-17 11:04:04 -0300

    lesson03

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,18 @@
 <script>
-	export let name;
+	let name = "Mariano";
+	let beltColour = 'black';
+	const handleClick = () => {
+		beltColour = 'orange';
+	};
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 	<p>Tremendo!!!!</p>
+	<p>que pasa?</p>
+	<p>{beltColour} belt</p>
+	<button on:click={handleClick}>update belt colour</button>
 </main>
 
 <style>
