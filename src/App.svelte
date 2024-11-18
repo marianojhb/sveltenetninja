1eeb23d462fa812ffc6ae69d8150d49cf5d61599
Mariano Belgrano <mbelgrano@gmail.com>
2024-11-17 23:55:06 -0300

    lesson13b

--- src/App.svelte
+++ src/App.svelte
@@ -40,7 +40,7 @@
   on:click={toggleModal}
 />
 <main>
-  <button on:click={toggleModal}>Open Modal...</button>
+  <button on:click|once={toggleModal}>Open Modal...</button>
   {#each people as person (person.id)}
     <div style="color: {person.beltcolour}">
       <h4>{person.name}</h4>
