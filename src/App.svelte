27c36d043875eb05775c77e4aa463668e656e216
Mariano Belgrano <mbelgrano@gmail.com>
2024-11-19 02:00:42 -0300

    lesson20

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,27 @@
 <script>
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
+	import Tabs from './components/shared/Tabs.svelte';
+
+  // tabs
+  let items = ['Current Poll', 'Add New Poll'];
+  let activeItem = 'Current Poll'
+
+  const tabChange = (e) => {
+    activeItem = e.detail
+  }
+
+
 </script>
 
 <Header />
 <main>
+  <Tabs {activeItem} {items} on:tabChange={tabChange}/>
+  {#if activeItem === 'Current Poll'}
+  <p>Poll list component goes here</p>
+  {:else if activeItem === 'Add New Poll'}
+  <p>New poll component goes here</p>
+  {/if}
 </main>
 <Footer />
 
