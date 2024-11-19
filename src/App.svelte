8dc42ac8c0d54afde222c6073555d4d6e8464c5f
Mariano Belgrano <mbelgrano@gmail.com>
2024-11-18 23:56:53 -0300

    lesson19

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,17 @@
 <script>
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
 </script>
 
+<Header />
 <main>
-	<h1>Hello, Ninjas!</h1>
 </main>
+<Footer />
 
 <style>
+	main{
+    width: 100%;
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
